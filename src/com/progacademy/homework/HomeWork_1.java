e7b6c66bd80a42aa2f04f6ac87d4dc94ba350682
dn270596pko <dp270596pko@gmail.com>
2022-02-07 21:50:23 +0200

    commit

---
+++ src/com/progacademy/homework/HomeWork_1.java
@@ -0,0 +1,22 @@
+package com.progacademy.homework;
+import java.util.Scanner;
+
+public class HomeWork_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите целое пятизначное число: ");
+        int number = scanner.nextInt();
+
+        int a = number / 10000;
+        int b = number % 10000 / 1000;
+        int c = number % 1000 / 100;
+        int d = number % 100 / 10;
+        int e = number % 10;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
