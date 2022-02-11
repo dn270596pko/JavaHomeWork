5e3538e526f7e9bd69a5cc0ad05a756c48cb8b51
kirillpankov <kirill.pankov@privatbank.ua>
2022-02-12 01:11:17 +0200

    Update

---
+++ src/com/progacademy/homework4/Hourglass.java
@@ -0,0 +1,15 @@
+package com.progacademy.homework4;
+
+import java.util.Scanner;
+
+public class Hourglass {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите ширину часов: ");
+        int width = scanner.nextInt();
+
+
+
+    }
+}
